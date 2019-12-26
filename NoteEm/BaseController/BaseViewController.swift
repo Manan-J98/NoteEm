e293faba3fa55d1c971454f654375e021ff526f5
Manan <manan.juneja@jungleworks.com>
2019-12-26 17:51:31 +0530

    basecontroller started

---
+++ NoteEm/BaseController/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  NoteEm
+//
+//  Created by Intern on 26/12/19.
+//  Copyright © 2019 Intern. All rights reserved.
+//
+
+import UIKit
+
+class _BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
